d90a40462193d7711fbe57f997ac96d11a89cacf
tanvirshhub <tanvirshuvo712@gmail.com>
2024-03-06 03:38:14 +0600

    new code added

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#hero-slider1, #hero-slider2, #hero-slider3').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+        // Add more options based on your requirements
+    });
+});
