e50bb60ce97ac186f7759ae53a822e668b8be586
tanvirshhub <tanvirshuvo712@gmail.com>
2024-03-07 04:02:46 +0600

    style complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
         speed: 500,
         fade: true,
         autoplay: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        nextArrow: false, // Set nextArrow to false to hide the next button
         // Add more options based on your requirements
     });
 });
